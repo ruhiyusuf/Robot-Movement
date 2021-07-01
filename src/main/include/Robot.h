6f2596dc6ecc21ead5b5a77311bbe5ac4ba424b6
Aditya Tomar <aditya26042005@gmail.com>
2021-07-01 00:44:40 -0700

    nulled out pointers

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -12,18 +12,18 @@
 class Robot : public frc::TimedRobot {
  public:
   static const int leftLeadDeviceID = 1, rightLeadDeviceID = 2, leftFollowDeviceID = 3, rightFollowDeviceID = 4; // replace with actual motor IDs
-  rev::CANSparkMax* m_leftLeadMotor; 
-  rev::CANSparkMax* m_rightLeadMotor; 
-  rev::CANSparkMax* m_leftFollowMotor; 
-  rev::CANSparkMax* m_rightFollowMotor; 
+  rev::CANSparkMax* m_leftLeadMotor = nullptr; 
+  rev::CANSparkMax* m_rightLeadMotor = nullptr; 
+  rev::CANSparkMax* m_leftFollowMotor = nullptr; 
+  rev::CANSparkMax* m_rightFollowMotor = nullptr; 
 
   double left_y = 0.0;
-  frc::XboxController* controller;
+  frc::XboxController* controller = nullptr;
 
   double distance = 0;
-  double l_wheel_rotations = 0.0, r_wheel_rotations = 0.0, l_wheel_circum = 0.0, r_wheel_circum = 0.0, l_wheel_dist = 0.0, r_wheel_dist = 0.0;
-  rev::CANEncoder* m_encoderSensor_left_motor;
-  rev::CANEncoder* m_encoderSensor_right_motor;
+  double l_motor_rots = 0.0, r_motor_rots = 0.0, l_motor_circum = 0.0, r_motor_circum = 0.0, l_motor_dist = 0.0, r_motor_dist = 0.0;
+  rev::CANEncoder* m_encoderSensor_left_motor = nullptr;
+  rev::CANEncoder* m_encoderSensor_right_motor = nullptr;
 
   void RobotInit() override;
   void RobotPeriodic() override;
