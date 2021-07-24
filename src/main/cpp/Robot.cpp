18dabc3ba99a6a9aab83a3200c8b5981861a1239
Aditya Tomar <aditya26042005@gmail.com>
2021-07-24 14:18:49 -0700

    moved motor initialization into Robot.h and removed pointers to encoders

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -7,25 +7,18 @@
 #include <math.h>
 
 void Robot::RobotInit() {
-  this->m_leftLeadMotor = new rev::CANSparkMax(leftLeadDeviceID, rev::CANSparkMax::MotorType::kBrushless);
-  this->m_rightLeadMotor = new rev::CANSparkMax(rightLeadDeviceID, rev::CANSparkMax::MotorType::kBrushless);
-  this->m_leftFollowMotor = new rev::CANSparkMax(leftFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless);
-  this->m_rightFollowMotor = new rev::CANSparkMax(rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless);
+  m_leftLeadMotor->RestoreFactoryDefaults();
+  m_rightLeadMotor->RestoreFactoryDefaults();
+  m_leftFollowMotor->RestoreFactoryDefaults();
+  m_rightFollowMotor->RestoreFactoryDefaults();
 
-  this->m_leftLeadMotor->RestoreFactoryDefaults();
-  this->m_rightLeadMotor->RestoreFactoryDefaults();
-  this->m_leftFollowMotor->RestoreFactoryDefaults();
-  this->m_rightFollowMotor->RestoreFactoryDefaults();
+  m_leftLeadMotor->SetInverted(false);
+  m_leftFollowMotor->Follow(*m_leftLeadMotor, false);
+  m_rightLeadMotor->SetInverted(true);
+  m_rightFollowMotor->Follow(*m_rightLeadMotor, false);
 
-  this->m_left = new frc::SpeedControllerGroup(*this->m_leftLeadMotor, *this->m_leftFollowMotor);
-  this->m_right = new frc::SpeedControllerGroup(*this->m_rightLeadMotor, *this->m_rightFollowMotor);
-
-  this->m_robotDrive = new frc::DifferentialDrive(*this->m_left, *this->m_right);
-
-  this->controller = new frc::XboxController{0}; // replace with USB port number on driver station
-
-  *m_encoderSensor_left_motor = this->m_leftLeadMotor->GetEncoder();
-  *m_encoderSensor_right_motor = this->m_rightLeadMotor->GetEncoder();        
+  m_leftLeadMotor->GetEncoder();
+  m_rightLeadMotor->GetEncoder();        
 }
 void Robot::RobotPeriodic() {
   frc::SmartDashboard::PutNumber("left y: ", left_y);
@@ -39,13 +32,13 @@ void Robot::RobotPeriodic() {
 void Robot::AutonomousInit() {
   max_speed = 0.5;
   setpoint = 5.0;     // feet
-  double wheel2GearR = (5.7 * M_PI) / (40 / 28); // 5.7 inch diameter wheel, 28 teeth on driver gear, 40 teeth on driven gear
-  m_encoderSensor_left_motor->SetPositionConversionFactor(wheel2GearR);
-  m_encoderSensor_right_motor->SetPositionConversionFactor(wheel2GearR);
+  double wheel2GearR = (5.7 * M_PI) / (40 / 14); // 5.7 inch diameter wheel, 28 teeth on driver gear, 40 teeth on driven gear
+  m_leftLeadMotor->GetEncoder().SetPositionConversionFactor(wheel2GearR);
+  m_rightLeadMotor->GetEncoder().SetPositionConversionFactor(wheel2GearR);
 }
 void Robot::AutonomousPeriodic() {
-  l_wheel_rots = m_encoderSensor_left_motor->GetPosition();
-  r_wheel_rots = m_encoderSensor_right_motor->GetPosition();
+  l_wheel_rots = m_leftLeadMotor->GetEncoder().GetPosition();
+  r_wheel_rots = m_rightLeadMotor->GetEncoder().GetPosition();
 
   l_wheel_dist = (l_wheel_rots * l_wheel_circum) / 12; 
   r_wheel_dist = (r_wheel_rots * r_wheel_circum) / 12;
